dd5a7b325cdbd39102e6813973f76cb3b46f45a0
Naoumi1214 <wada121400@gmail.com>
2020-03-14 16:55:55 +0900

    3-2完了

--- frontproject/webpack.mix.js
+++ frontproject/webpack.mix.js
@@ -1,3 +1,6 @@
 let mix = require('laravel-mix');
 
-mix.js('src/app.js', 'public/js');
+mix.js('src/app.js', 'public/js')
+    .sass('src/sass/app.scss', 'public/css', {
+        implementation: require('node-sass')
+    });
